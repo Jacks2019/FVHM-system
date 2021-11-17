a3e46e7f692a4b6ee5cb62a6022c466053c1b64a
Mr.Yang <1351787345@qq.com>
2021-11-17 11:13:25 +0800

    修改网站图标

--- src/router.js
+++ src/router.js
@@ -10,45 +10,27 @@ const configs = [
   {
     path: '/', // 路由地址，children 内路由地址 = 上一嵌套的路由地址（若无则为""） + 该path
     label: '首页', // 【必选】显示在菜单栏里的名字
-    name: 'one', // 【必选】一定要有，且保证在 configs 内唯一
+    name: 'homepage', // 【必选】一定要有，且保证在 configs 内唯一
     icon: 'el-icon-s-comment', // 显示在菜单栏内的 icon
     component: homepage // 页面组件
   },
   {
-    path: '/a',
-    label: '监测信息',
-    icon: 'el-icon-s-opportunity',
-    name: 'second',
-    component: Example2,
-  },
-  {
-    path: '/b',
-    label: '用水用电统计',
-    icon: 'el-icon-s-data',
-    name: 'three',
-    component: Example3,
-  },
-  {
-    path: '/c',
+    path: '/map',
     label: '一张图',
-    icon: 'el-icon-map-location',
-    name: 'four',
-    component: Example4,
-  },
-  {
-    path: '/manage',
-    label: '用户类型管理',
-    icon: 'el-icon-map-location',
-    name: 'manage',
-    hide: true,
-    component: Manage
-  },
-  {
-    path: '/manage/edit',
-    label: '用户类型权限管理',
-    icon: 'el-icon-map-location',
-    name: 'manageEdit',
-    component: EditFunction
+    icon: 'el-icon-s-opportunity',
+    name: 'map',
+    children:[
+      {
+        path:'/content',
+        label:'内容',
+        component: Example2
+      },
+      {
+        path:'/detail',
+        label:'详情',
+        component: Example3
+      }
+    ]
   }
 ];
 
